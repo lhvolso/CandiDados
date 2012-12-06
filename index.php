ee9f660f073b2c9238611a614153a5a99947f6fe
Luiz Henrique Volso <lhvolso@gmail.com>
2012-12-06 18:57:41 -0200

    Estilização Página Inicial

--- index.php
+++ index.php
@@ -8,34 +8,41 @@
 	<meta name="author" content="Luiz Henrique Volso">
 	<meta name="robots" content="noindex">
 	<link rel="stylesheet" href="css/reset.css">
+	<link rel="stylesheet" href="css/layout.css">
+	<link href="http://fonts.googleapis.com/css?family=Titillium+Web:400,200,700" rel="stylesheet" type="text/css">
 </head>
 <body>
 	<header>
-		<h1>CandiDados.com.br</h1>
-		<div id="publicidade"></div>
-		<form action="#">
-			<input type="text" name="busca">
-			<button type="submit">Buscar</button>
-		</form>
+		<div class="centro">
+			<h1 class="logo">CANDI<strong>DADOS</strong></h1>
+			<p class="infobusca">Pesquise pelo nome do candidato, ano da eleição, local ou cargo:</p>
+			<form action="#" class="busca">
+				<input type="text" name="busca" placeholder="ex.: Nome do candidato Paraná 2010">
+				<button type="submit">Buscar</button>
+			</form>
+		</div>
 	</header>
-	<section>
-		<h2>Sobre o CandiDados</h2>
-		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin non nibh vel odio accumsan posuere at id magna. Donec nec eros id ipsum hendrerit gravida. Curabitur consectetur nisl ut sem vulputate non aliquam libero egestas. Integer iaculis lorem ut metus volutpat in semper urna congue. In hac habitasse platea dictumst. Duis venenatis vulputate nunc, et dapibus velit blandit quis. Phasellus convallis dapibus sem eu posuere. Donec dolor velit, tempor ac iaculis eu, eleifend nec massa. Aliquam et nisl nec lacus pellentesque volutpat nec et massa.</p>
-	</section>
-	<section>
-		<h2>Sobre Dados Abertos</h2>
-		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin non nibh vel odio accumsan posuere at id magna. Donec nec eros id ipsum hendrerit gravida. Curabitur consectetur nisl ut sem vulputate non aliquam libero egestas. Integer iaculis lorem ut metus volutpat in semper urna congue. In hac habitasse platea dictumst. Duis venenatis vulputate nunc, et dapibus velit blandit quis. Phasellus convallis dapibus sem eu posuere. Donec dolor velit, tempor ac iaculis eu, eleifend nec massa. Aliquam et nisl nec lacus pellentesque volutpat nec et massa.</p>
-	</section>
+	<div class="centro inicio">
+		<section class="fleft">
+			<h2 class="tituloazul">Sobre o CandiDados</h2>
+			<p>Layout com conceito minimalista com foco no conteúdo, em uma analogia a <strong>dados abertos</strong>, a fonte tipográfica é de <strong>uso livre</strong>, assim como a imagem do background e os ícones, os elementos abrem e não fecham (com exceção do imput e botões, e do software que utilizei).</p>
+		</section>
+		<section class="fright">
+			<h2 class="tituloazul">Sobre Dados Abertos</h2>
+			<p>As cores irão operar segundo um esquema de conteúdo estático ou interagível, que demanda de ação do usuário, títulos de textos utilizarão variações da cor azul seguindo uma hierarquia do mais forte para o mais claro, links, botões, inputs terão variações do laranja.</p>
+		</section>
+	</div>
 	<footer>
-		<nav>
-			<ul>
-				<li><a href="#">Sobre o projeto</a></li>
-				<li><a href="#">Sobre dados abertos</a></li>
-				<li><a href="#">Equipe</a></li>
-				<li><a href="#">Glossário</a></li>
-				<li><a href="#">Ajuda</a></li>
-			</ul>
-		</nav>
+		<div class="centro">
+			<nav>
+				<ul>
+					<li><a href="#">o projeto</a></li>
+					<li><a href="#">os desenvolvedores</a></li>
+					<li><a href="#">glossário</a></li>
+					<li><a href="#">ajuda</a></li>
+				</ul>
+			</nav>
+		</div>
 	</footer>
 </body>
 </html>
